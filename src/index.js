5b7634facf38ead784dc7423c996609cbf97d79d
Yurii Yashchuk <106056914+IronFather@users.noreply.github.com>
2022-12-23 06:03:20 +0200

    Revert "updated 1"
    This reverts commit 5812db23da5af23b76f44f3359a1b3bb3860676a.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,21 @@ import getRefs from './get-refs';
 
 const axios = require('axios').default;
 const refs = getRefs();
+const BASE_URL = `https://pixabay.com/api/`;
+// let fields = [`name,capital,population,flags,languages`];
 
+// key - твой уникальный ключ доступа к API.
+// q - термин для поиска. То, что будет вводить пользователь.
+// image_type - тип изображения. Мы хотим только фотографии, поэтому задай значение photo.
+// orientation - ориентация фотографии. Задай значение horizontal.
+// safesearch - фильтр по возрасту. Задай значение true.
 
-console.log(refs.searchFormEl);
-console.log(refs.inputEl);
-console.log(refs.galleryEl);
-console.log(refs.loadMoreBtnEl);
+// console.log(refs.searchFormEl);
+// console.log(refs.inputEl);
+// console.log(refs.galleryEl);
+// console.log(refs.loadMoreBtnEl);
+
+refs.loadMoreBtnEl.classList.add(`is-hidden`);
 
 refs.searchFormEl.addEventListener(`submit`, onSearchFormSubmit);
 
@@ -20,8 +29,39 @@ function onSearchFormSubmit() {
 
 }
 
+function fetchPhotoCards() {
+  // const url = `${BASE_URL}/?${fields}`; 
+  
+  return fetch(url)
 
+}
+// function makeMarkupOnePhotoCard() {
+//   return `
+//   <div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>`
+// }
 
+// renderPhotoCards() {
+// };
 
+// function renderPhotoCards(markup = '') {
+//   return (refs.galleryEl.innerHTML = markup);
+// }
 
+// refs.loadMoreBtnEl.classList.remove(`is-hidden`);
 
